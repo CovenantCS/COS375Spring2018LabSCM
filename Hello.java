b4f14ae42a7afae5d6b1d54d55d5ef820594b89e
Mourice008 <47871213+Mourice008@users.noreply.github.com>
2019-02-22 11:52:02 -0500

    Version 1.0
    Changed "Hello World" to "Hello Michael Northfield"

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@
 //This class will print "Hello World"
 public class Hello {
   public static void main(String args[]){
-    System.out.println("Hello Michael");
+    System.out.println("Hello Michael Northfield");
   }
 }
